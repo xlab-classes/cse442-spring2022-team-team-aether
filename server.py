407c8df89bb08805ed11fe98fc52dda427eb9758
Max <mskravec@buffalo.edu>
2022-02-27 16:09:39 -0500

    Added links between all server pages Stubbed out java control form submit Removed proto login

--- server.py
+++ server.py
@@ -1,12 +1,28 @@
 import os
 import re
-from flask import Flask, render_template
+import flask
+from flask import Flask, render_template, send_from_directory
 app = Flask(__name__)
 
 
+@app.route("/index")
 @app.route("/")
 def root():
-    return render_template('index.html')
+    return render_template("index.html")
+
+
+@app.route("/make")
+def make():
+    return render_template("make.html")
+
+
+@app.route('/static/frontEngine')
+def send_engine(path):
+    return send_from_directory('js', path)
+
+@app.route('/static/styles')
+def send_styles(path):
+    return send_from_directory('css', path)
 
 @app.route("/login")
 def login():
@@ -24,13 +40,10 @@ def popular():
 def account():
     return render_template('account.html')
 
-@app.route("/styles")
-def styles():
-    return render_template('styles.css')
-
 @app.route("/search")
 def search():
     return render_template('search.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ static/frontEngine.js
@@ -0,0 +1,16 @@
+const url = "http://127.0.0.1:5000/";
+
+var xrequest = new XMLHttpRequest();
+
+
+xrequest.open("POST", url);
+xrequest.setRequestHeader("Accept", "application/json");
+xrequest.setRequestHeader("Content-Type", "application/json");
+
+
+function submitdata(){
+    var name = document.getElementById("username");
+    var pass = document.getElementById("password");
+    var payload = { "username" : name, "password" : pass };
+    xrequest.send(JSON.stringify(payload));
+}
