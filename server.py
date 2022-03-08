e186ee62b18527d57894ed9619e453dfb72fc082
Max <mskravec@buffalo.edu>
2022-03-08 12:12:17 -0500

    implemented front end login data submission. removed use of front engine for login purposes. added POST request route to the /login path

--- server.py
+++ server.py
@@ -1,7 +1,4 @@
-import os
-import re
-import flask
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
 app = Flask(__name__)
 
 
@@ -10,11 +7,9 @@ app = Flask(__name__)
 def root():
     return render_template("index.html")
 
-
 @app.route("/make")
 def make():
     return render_template("make.html")
-
 
 @app.route('/static/frontEngine')
 def send_engine(path):
@@ -24,10 +19,14 @@ def send_engine(path):
 def send_styles(path):
     return send_from_directory('css', path)
 
-@app.route("/login")
+@app.route("/login", methods=["GET","POST"])
 def login():
-    return render_template('login.html')
-
+    if request.method == "GET":
+        return render_template('login.html')
+    elif request.method == "POST":
+        data = request.form.to_dict()
+        print(data)
+        return render_template('account.html')
 @app.route("/createaccount")
 def createaccount():
     return render_template('createaccount.html')
