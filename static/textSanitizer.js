060d37766adca64336cea76e35e61aa07038fca8
jbrown49 <jbrown49@buffalo.edu>
2022-03-14 00:43:54 -0400

    Added text sanitizer function and demo webpage #23

---
+++ static/textSanitizer.js
@@ -0,0 +1,22 @@
+// This will sanitize text of foreign JavaScript code and SQL commands
+
+
+function sanitizeData(text) {
+    var cleanText = text;
+    // General syntax breaker
+    cleanText = cleanText.replaceAll(";", "");
+    // Start with removing JavaScript
+    cleanText = cleanText.replaceAll("<script>", "");
+    cleanText = cleanText.replaceAll("</script>", "");
+    // Finish off with SQL commands
+    cleanText = cleanText.replaceAll("SELECT", "");
+    cleanText = cleanText.replaceAll("select", "");
+    cleanText = cleanText.replaceAll("CREATE", "");
+    cleanText = cleanText.replaceAll("create", "");
+    cleanText = cleanText.replaceAll("UPDATE", "");
+    cleanText = cleanText.replaceAll("update", "");
+    cleanText = cleanText.replaceAll("DELETE", "");
+    cleanText = cleanText.replaceAll("delete", "");
+    alert(cleanText); // For testing purposes
+    return cleanText;
+}
