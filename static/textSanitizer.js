dbd068538f8aa3836b5d3850474e8c66e0195477
jbrown49 <jbrown49@buffalo.edu>
2022-03-14 11:24:37 -0400

    Fixed recursive exploit #23

--- static/textSanitizer.js
+++ static/textSanitizer.js
@@ -5,18 +5,22 @@ function sanitizeData(text) {
     var cleanText = text;
     // General syntax breaker
     cleanText = cleanText.replaceAll(";", "");
-    // Start with removing JavaScript
-    cleanText = cleanText.replaceAll("<script>", "");
-    cleanText = cleanText.replaceAll("</script>", "");
-    // Finish off with SQL commands
-    cleanText = cleanText.replaceAll("SELECT", "");
-    cleanText = cleanText.replaceAll("select", "");
-    cleanText = cleanText.replaceAll("CREATE", "");
-    cleanText = cleanText.replaceAll("create", "");
-    cleanText = cleanText.replaceAll("UPDATE", "");
-    cleanText = cleanText.replaceAll("update", "");
-    cleanText = cleanText.replaceAll("DELETE", "");
-    cleanText = cleanText.replaceAll("delete", "");
+    do {
+        // Start with removing JavaScript
+        cleanText = cleanText.replaceAll("<script>", "");
+        cleanText = cleanText.replaceAll("</script>", "");
+        // Finish off with SQL commands
+        cleanText = cleanText.replaceAll("SELECT", "");
+        cleanText = cleanText.replaceAll("select", "");
+        cleanText = cleanText.replaceAll("CREATE", "");
+        cleanText = cleanText.replaceAll("create", "");
+        cleanText = cleanText.replaceAll("UPDATE", "");
+        cleanText = cleanText.replaceAll("update", "");
+        cleanText = cleanText.replaceAll("DELETE", "");
+        cleanText = cleanText.replaceAll("delete", "");
+    } while (cleanText.includes("<script>") && cleanText.includes("</script>") &&
+    	cleanText.toLowerCase().includes("select") && cleanText.toLowerCase().includes("create") &&
+    	cleanText.toLowerCase().includes("update") && cleanText.toLowerCase().includes("delete"));
     alert(cleanText); // For testing purposes
     return cleanText;
 }
