ae624da13bd40aa5fc5e15e1abc57a713fe7bb01
vas1295 <54868050+vas1295@users.noreply.github.com>
2022-04-11 19:15:07 -0400

    storing created memes into database

--- imagestore.py
+++ imagestore.py
@@ -8,19 +8,24 @@ db = mysql.connector.connect(
 )
 
 
-def imgstore(username, imgbyte): 
-    cursor = db.cursor()
-    cr = "CREATE TABLE IF NOT EXISTS imgstore username varchar(64), imgs varbinary(max) foreign key (username) references users (username) "
-    cursor.execute(cr)
-    db.commit()
+def imgstore(username, imgname, imgbyte): 
+  cursor = db.cursor()
+  cr = "CREATE TABLE IF NOT EXISTS imgstore (username varchar(64), img_name varchar(64), img_byte varbinary(max) foreign key (username) references users (username) "
+  cursor.execute(cr)
+  db.commit()
 
-    insVal = "INSERT INTO imgstore (username, imgbyte) VALUES (%s, %s)"
-    val = [username, imgbyte]
-    print(val)
-    cursor.execute(insVal, val)
-    db.commit()
-    return True
+  insVal = "INSERT INTO imgstore (username, img_name, img_byte) VALUES (%s, %s, %s)"
+  val = [username, imgname, imgbyte]
+  print(val)
+  cursor.execute(insVal, val)
+  db.commit()
+  return True
 
+###
+# def getimg(username, imgname): 
+#   cursor = db.cursor()
+#   query = "SELECT img_byte FROM imgstore WHERE username = %s AND img_name = %s"
+#   cursor.execute(query, (username, imgname))
 
 
   
